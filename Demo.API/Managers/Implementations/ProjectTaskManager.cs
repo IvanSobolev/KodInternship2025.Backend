be47c7bc96767810b68bb5d0e81822313e9543ca
IvanSobolev <i@bezlikiy.ru>
2025-05-15 04:55:56 +0300

    feat: kafka test

--- Demo.API/Managers/Implementations/ProjectTaskManager.cs
+++ Demo.API/Managers/Implementations/ProjectTaskManager.cs
@@ -2,13 +2,16 @@
 using Demo.DAL.Dto;
 using Demo.DAL.Enums;
 using Demo.DAL.Repositories.Interfaces;
+using Demo.Kafka;
 using Demo.Managers.Interfaces;
 
 namespace Demo.Managers.Implementations;
 
-public class ProjectTaskManager(IProjectTaskRepository repository) : IProjectTaskManager
+public class ProjectTaskManager(IProjectTaskRepository repository, IKafkaProducerService kafkaProducer, IWorkerRepository workerRepository) : IProjectTaskManager
 {
     private readonly IProjectTaskRepository _repository = repository;
+    private readonly IKafkaProducerService _kafkaProducer = kafkaProducer;
+    private readonly IWorkerRepository _workerRepository = workerRepository;
     
     public async Task<Result<ProjectTaskDto?>> GetByIdAsync(Guid id)
     {
@@ -32,7 +35,62 @@ public class ProjectTaskManager(IProjectTaskRepository repository) : IProjectTas
 
     public async Task<Result<ProjectTaskDto>> AddAsync(AddProjectTaskDto newTask)
     {
-        return await _repository.AddAsync(newTask.Title, newTask.Text, newTask.Department);
+         var addResult = await _repository.AddAsync(newTask.Title, newTask.Text, newTask.Department);
+
+        if (!addResult.IsSuccess)
+        {
+            Console.WriteLine("Ошибка при добавлении задачи в БД: {Error}", addResult.Error);
+            return addResult;
+        }
+
+        var createdTaskDto = addResult.Value;
+        List<long> recipientTelegramIds = new List<long>();
+        var workersResult = await _workerRepository.GetTelegramIdInDepartment(createdTaskDto.Department);
+        try
+        {
+            if (workersResult.IsSuccess)
+            {
+                recipientTelegramIds.AddRange(workersResult.Value);
+            }
+            else
+            {
+                Console.WriteLine("Не удалось получить список работников для уведомления о задаче {TaskId}: {Error}",
+                    createdTaskDto.Id, workersResult.Error);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка при получении списка работников для уведомления о задаче {TaskId}", createdTaskDto.Id);
+        }
+
+        
+        if (recipientTelegramIds.Any())
+        {
+            var kafkaMessage = new NewTaskMessage
+            {
+                TaskId = createdTaskDto.Id,
+                Title = createdTaskDto.Title,
+                Text = createdTaskDto.Text,
+                Department = createdTaskDto.Department.ToString(),
+                RecipientTelegramIds = recipientTelegramIds
+            };
+
+            try
+            {
+                await _kafkaProducer.ProduceNewTaskMessageAsync(kafkaMessage);
+                Console.WriteLine("Сообщение о новой задаче {TaskId} успешно отправлено в Kafka.", createdTaskDto.Id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка при отправке сообщения о новой задаче {TaskId} в Kafka.", createdTaskDto.Id);
+            }
+        }
+        else
+        {
+           Console.WriteLine("Нет получателей для уведомления о новой задаче {TaskId}. Сообщение в Kafka не отправлялось.", createdTaskDto.Id);
+        }
+
+        return Result<ProjectTaskDto>.Success(createdTaskDto);
     }
 
     public async Task<Result<ProjectTaskDto>> UpdateAsync(UpdateTaskDto updateTask)
